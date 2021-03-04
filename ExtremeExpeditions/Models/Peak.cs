44c5aa7929f5cc4b3385da69076de21e091fa546
Erik Öberg <erik.oberg@miun.se>
2021-03-04 11:10:30 +0100

    Add project files.

---
+++ ExtremeExpeditions/Models/Peak.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtremeExpeditions
+{
+    /// <summary>
+    /// Mountain
+    /// </summary>
+    public class Peak
+    {
+        /// <summary>
+        /// Primary key
+        /// </summary>
+        public int PeakId { get; set; }
+        /// <summary>
+        /// Name of peak
+        /// </summary>
+        public string PeakName { get; set; }
+        /// <summary>
+        /// Height in meters over sea level
+        /// </summary>
+        public int? Elevation { get; set; }
+        /// <summary>
+        /// Foreign key from table Range
+        /// </summary>
+        public int? RangeId { get; set; }
+
+        public override string ToString()
+        {
+            return PeakName;
+        }
+    }
+}
---
+++ ExtremeExpeditions/Repositories/ExpeditionRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+
+namespace ExtremeExpeditions.Repositories
+{
+    public class ExpeditionRepository
+    {
+        private static readonly string connectionString = "Server=localhost;Port=5433;Database=expeditiondemodb;User ID=demouser; Password=Hemligt;";
+
+        #region Read
+        /// <summary>
+        /// Gets a peak from database
+        /// </summary>
+        /// <param name="id">Unique primary key</param>
+        /// <returns>Peak</returns>
+        public Peak GetPeak(int id)
+        {
+            string stmt = "select * from peaks where peakid = @id";
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            using var command = new NpgsqlCommand(stmt, conn);
+
+            command.Parameters.AddWithValue("id", id);
+
+            using var reader = command.ExecuteReader();
+            Peak peak = null;
+            while (reader.Read())
+            {
+                peak = new Peak
+                {
+                    PeakId = (int)reader["peakid"],
+                    PeakName = (string)reader["peakname"],
+                    Elevation=(int)reader["elevation"],
+                    RangeId=Convert.IsDBNull(reader["rangeid"]) ? null : (int?)reader["rangeid"]
+                };
+            }
+            //return peak;
+
+            return new Peak
+            {
+                PeakId = 123,
+                PeakName = "Erik"
+            };
+        }
+
+        /// <summary>
+        /// Gets a list of all peaks
+        /// </summaryList of Peak</returns>
+        public List<Peak> GetPeaks()
+        {
+            string stmt = "select * from peaks";
+            using var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            using var command = new NpgsqlCommand(stmt, conn);
+            using var reader = command.ExecuteReader();
+            Peak peak = null;
+            var peaks = new List<Peak>();
+            while (reader.Read())
+            {
+                peak = new Peak
+                {
+                    PeakId = (int)reader["peakid"],
+                    PeakName = (string)reader["peakname"],
+                    Elevation = (int)reader["elevation"],
+                    RangeId = Convert.IsDBNull(reader["rangeid"]) ? null : (int?)reader["rangeid"]
+                };
+                peaks.Add(peak);
+            }
+            return peaks;
+        }
+
+
+        #endregion
+    }
+}
